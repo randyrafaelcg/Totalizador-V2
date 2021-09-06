051ed541b67b0fc0db214d317cde1eb10c5d6f28
randyrafaelcg <randyrafaelcg@gmail.com>
2021-09-06 15:45:26 -0400

    Se agrega la visualizacion del total con el impuesto que corresponde segun el estado elegido

--- src/Totalizador.js
+++ src/Totalizador.js
@@ -7,11 +7,11 @@ formT.addEventListener("submit", (event) => {
     event.preventDefault();
     //alert("Cantidad indtorucida: "+ cantidad.value + " Precio introducido: "+ precio.value);
     let totalMult = precioTotal(cantidad.value, precio.value);
-    //let totalConIm=ImpFORstate(state.value);
+    let totalConIm=ImpFORstate(state.value);
     //let totaldes= Descuento();
     document.getElementById('total').innerHTML = totalMult;
     //document.getElementById('descuento').innerHTML = totaldes;
-    //document.getElementById('totalConImpuesto').innerHTML = totalConIm;
+    document.getElementById('totalConImpuesto').innerHTML = totalConIm;
 });
 
 function precioTotal(cantidad, precio) {
